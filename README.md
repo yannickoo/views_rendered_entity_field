a03c533ca404960d1a57e15303afef350e39b14a
yannickoo <github@yannick.md>
2015-11-15 01:01:15 +0100

    :hatching_chick:

---
+++ README.md
@@ -0,0 +1,13 @@
+# Views Rendered Entity Field
+
+> Allows you to add attributes to your menu links.
+
+## Installation
+
+1. Install the module the [drupal way](https://www.drupal.org/documentation/install/modules-themes/modules-8)
+2. Add a *Rendered entity* field to your view
+3. Select the view mode
+
+## Contributing
+
+Pull requests and stars are always welcome. For bugs and feature requests, [please create an issue](https://github.com/yannickoo/views_rendered_entity_field/issues/new).
---
+++ src/Plugin/views/field/EntityRendered.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\views_rendered_entity_field\Plugin\views\field\EntityRendered.
+ */
+
+namespace Drupal\views_rendered_entity_field\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * A handler to provide a field for rendered entity.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("entity_rendered")
+ */
+ class EntityRendered extends FieldPluginBase {
+   /**
+    * {@inheritdoc}
+    */
+   public function query() {}
+
+  /**
+   * {@inheritdoc}
+   */
+   protected function defineOptions() {
+     $options = parent::defineOptions();
+     $options['view_mode'] = ['default' => 'default'];
+
+     return $options;
+   }
+
+  /**
+   * {@inheritdoc}
+   */
+   public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+     parent::buildOptionsForm($form, $form_state);
+
+     $view_modes = [];
+     $definitions = \Drupal::entityManager()->getDefinitions();
+
+     // @TODO: Figure out how to get entity type.
+
+     // Build entity types and view modes options.
+     foreach ($definitions as $entity_type => $definition) {
+       if ($definition->getGroup() == 'content') {
+         $view_modes[$entity_type] = \Drupal::entityManager()->getViewModeOptions($entity_type);
+       }
+     }
+
+     $form['view_mode'] = array(
+       '#type' => 'select',
+       '#title' => $this->t('View mode'),
+       '#required' => TRUE,
+       '#options' => $view_modes,
+       '#default_value' => isset($this->configuration['view_mode']) ? $this->configuration['view_mode'] : '',
+     );
+   }
+
+  /**
+   * {@inheritdoc}
+   */
+   public function render(ResultRow $row) {
+     $entity = $this->getEntity($row);
+     $entity_type = $entity->getEntityTypeId();
+     $view_mode = $this->options['view_mode'];
+     $render_controller = \Drupal::entityManager()->getViewBuilder($entity_type);
+     $build = $render_controller->view($entity, $view_mode);
+
+     return $build;
+   }
+
+}
